2dacc25805d883ef4f543dc8c74f144cedfe7d36
Matheusprower <matheusprower@gmail.com>
2026-05-27 20:16:59 -0300

    Criado a classe para testes

--- atividade/src/main/java/com/example/CalculadoraDesconto.java
+++ atividade/src/main/java/com/example/CalculadoraDesconto.java
@@ -3,8 +3,19 @@ package com.example;
 public class CalculadoraDesconto {
 
     public double calcularValorFinal(double valorCompra) {
-
-
-        
+        double valorComDesconto;
+        if(valorCompra >= 0) {
+            if(valorCompra < 100) {
+            return valorCompra;
+        } else if(valorCompra <= 100 && valorCompra >= 499) {
+            valorComDesconto = valorCompra - (valorCompra*0.05);
+            return valorComDesconto;
+        } else {            
+            valorComDesconto = valorCompra - (valorCompra*0.1);
+            return valorComDesconto;
+        }
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
